a18bb81191bc25a491b2d3620029fbfa10688c94
ItamarColler <itamar.koller@gmail.com>
2021-03-05 15:37:41 +0200

    modifying Navigation in build grade as expected from Menache presentation 6 A+B, adding Post Icon and Enabling Top ActionBar at the top of the Application

--- app/src/main/java/com/gather/land/activities/MainActivity.java
+++ app/src/main/java/com/gather/land/activities/MainActivity.java
@@ -2,6 +2,9 @@ package com.gather.land.activities;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.gather.land.R;
 import com.gather.land.models.Post;
@@ -10,6 +13,7 @@ import com.gather.land.models.User;
 import com.gather.land.reposetories.RepositoryApp;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.navigation.NavController;
@@ -20,7 +24,7 @@ import androidx.navigation.ui.NavigationUI;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    public NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,9 +35,10 @@ public class MainActivity extends AppCompatActivity {
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+        NavigationUI.setupActionBarWithNavController(this,navController);
 
         User user=new User("te22s@gmail.com","moti","last","empty",System.currentTimeMillis());
         RepositoryApp.getInstance(this).insertUser(user);
@@ -53,4 +58,20 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==android.R.id.home)
+        {
+            navController.navigateUp();
+            return true;
+        }
+        return NavigationUI.onNavDestinationSelected(item,navController);
+    }
 }
